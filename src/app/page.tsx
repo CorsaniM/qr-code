25665f68e9349cacbc3244071eb879b50c437a16
mariano cosco <135845409+marianoCosco@users.noreply.github.com>
2024-08-23 08:12:53 -0300

    delete main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,11 @@
 "use client"
 
-import { useRouter } from "next/navigation";
-import { api } from "~/trpc/react";
-
+import Link from "next/link";
 export default function Page() {
-
-const { mutateAsync: CreateParticipantes, isPending} = api.participants.create.useMutation()
-
-const participantes = api.participants.list.useQuery().data
-
-const  { mutateAsync: deleteP} = api.participants.delete.useMutation({})
-
-
-const router = useRouter();
-
-  async function HandleCreate() {
-
-
-
-
-    await CreateParticipantes({
-      name: "Fran",
-      lastname: "Marmo"
-    })
-    router.refresh();
-  }
-
-async function Delete(Id: number) {
-
-  await deleteP({
-      id: Id
-  })
-  
-  router.refresh()
-
-}
-
   return (
       <main className="flex min-h-screen flex-col items-center justify-center">
+      <Link href={"/integrantes"}> Home</Link>
       <h1>Hola</h1>
-
-
-      <button className="bord" disabled={isPending} onClick={HandleCreate}>crear participantes</button>
-
-
-      <ul>
-        {participantes ? participantes!.map((integrante) => {
-          return(
-          <div>
-            <li>
-                <h1>{integrante.id} {integrante.name} {integrante.lastname}</h1>
-                <button className="bord" disabled={isPending} onClick={() => Delete(integrante.id)}>Borrar participantes</button>
-            </li>
-          </div>
-          )
-        }) : (<h1>No existen participantes</h1>)}
-      </ul>
-
-
       </main>
   );
 }
